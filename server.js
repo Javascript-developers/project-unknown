c9129edf2cf64bf2e089e68bf59fa39288f793b0
quatre29 <quatre29@gmail.com>
2021-03-23 14:44:11 +0000

    mongoDB added to project

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const app = require("./app");
-const dotenv = require("dotenv");
+const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
