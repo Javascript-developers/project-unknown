d2a3e75e4670ee9d1b0b0501b5029b3d3dac97ce
quatre29 <quatre29@gmail.com>
2021-05-03 21:52:40 +0100

    added material ui to login page

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -2,7 +2,38 @@ import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import AuthContext from '../../context/auth/authContext';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const Login = (props) => {
+  const classes = useStyles();
+
   const authContext = useContext(AuthContext);
 
   const { login, isAuthenticated } = authContext;
@@ -45,29 +76,51 @@ const Login = (props) => {
   };
 
   return (
-    <Container>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <input
-            required
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            required
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-          />
-        </div>
-        <input type="submit" value="Login" />
+    <Container maxWidth="xs">
+      {/* <CssBaseline /> */}
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+      </div>
+      <form onSubmit={onSubmit} className={classes.form}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={onChange}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          type="password"
+          label="Password"
+          name="password"
+          autoComplete="password"
+          autoFocus
+          value={password}
+          onChange={onChange}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign In
+        </Button>
       </form>
     </Container>
   );
@@ -75,4 +128,4 @@ const Login = (props) => {
 
 export default Login;
 
-const Container = styled.div``;
+// const Container = styled.div``;
